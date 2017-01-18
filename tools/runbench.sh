db939a582149bdc825d644af0bc8009847815083
Brian Atkinson <nairb774@gmail.com>
2017-01-17 21:53:42 -0600

    Update to go benchcmp

--- tools/runbench.sh
+++ tools/runbench.sh
@@ -4,9 +4,24 @@ set -ex
 
 cd "${0%/*}/.."
 
-A="$PWD/build/benchmarks.$(git rev-parse --short --verify "$1")"
-B="$PWD/build/benchmarks.$(git rev-parse --short --verify "$2")"
+function run() {
+  echo "Starting run for $1"
+  if [ ! -e "$PWD/build/bench.$1" ]; then
+    rm "$PWD/build/bench.$1.tmp" || true
+    for I in $(seq 1 3); do
+      for f in "$PWD/build/benchmarks.$1/"*; do
+        "$f"
+      done
+    done | tee "$PWD/build/bench.$1.tmp"
+    mv "$PWD/build/bench.$1.tmp" "$PWD/build/bench.$1"
+  fi
+  echo "Finished run for $1"
+}
 
-for f in "$A/"*; do
-  "$PWD/tools/benchcmp" "$f" "$B/$(basename "$f")"
-done
+A="$(git rev-parse --short --verify "$1")"
+B="$(git rev-parse --short --verify "$2")"
+
+run "$A"
+run "$B"
+
+~/gocode/bin/benchcmp -best "$PWD/build/bench.$A" "$PWD/build/bench.$B"
