bdf4fdbf1ab2994d59d3f242dd4b5934c256717b
Brian Atkinson <nairb774@gmail.com>
2017-01-18 19:39:06 -0600

    more runbenchmark work

--- tools/runbench.sh
+++ tools/runbench.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-# set -ex
+set -ex
 
 cd "${0%/*}/.."
 
 function run1() {
   echo "Starting run for $1"
   for f in "$PWD/build/benchmarks.$1/"*; do
-    "$f" || exit
+    "$f"
   done | tee >(gzip -9 - > "$PWD/build/bench.$1.tmp")
   echo "Finished run for $1"
   cat "$PWD/build/bench.$1.tmp" >> "$PWD/build/bench.$1"
+  rm "$PWD/build/bench.$1.tmp"
 }
 
 A="$(git rev-parse --short --verify "$1")"
@@ -21,6 +22,6 @@ for I in $(seq 1 3); do
   run1 "$B"
 done
 
-~/gocode/bin/benchcmp -best \
-    <(gzip -d "$PWD/build/bench.$A") \
-    <(gzip -d "$PWD/build/bench.$B")
+~/gocode/bin/benchcmp -best -changed \
+    <(gzip -cd "$PWD/build/bench.$A") \
+    <(gzip -cd "$PWD/build/bench.$B")
